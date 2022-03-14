9c763db32369765d48733733cf99b4acc1746feb
Ononim <tzinatullin16@gmail.com>
2022-03-14 21:59:36 +0300

    departments.py

--- data/departments.py
+++ data/departments.py
@@ -2,15 +2,15 @@ import sqlalchemy
 from sqlalchemy import orm
 from sqlalchemy_serializer import SerializerMixin
 
-from db_session import SqlAlchemyBase
+from .db_session import SqlAlchemyBase
 
 
 class Department(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'departments'
     id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, primary_key=True)
-    title = sqlalchemy.Column(sqlalchemy.String)
-    chief = sqlalchemy.Column(sqlalchemy.Integer)
-    members = sqlalchemy.Column(sqlalchemy.String)
-    email = sqlalchemy.Column(sqlalchemy.String)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    chief = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    members = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
 
     user = orm.relation('User')
